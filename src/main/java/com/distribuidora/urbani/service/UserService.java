3cfe358ed62d07d634559e27463d72e971de31a1
SantiagoPividori <pividorisantiago@gmail.com>
2026-02-06 17:36:40 -0300

    feat(user): Implements UserDetailsService in UserService for login users

--- src/main/java/com/distribuidora/urbani/service/UserService.java
+++ src/main/java/com/distribuidora/urbani/service/UserService.java
@@ -1,7 +1,26 @@
 package com.distribuidora.urbani.service;
 
+import com.distribuidora.urbani.entity.User;
+import com.distribuidora.urbani.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+//Implementamos UserDetailsService para poder
+
 @Service
-public class UserService {
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
