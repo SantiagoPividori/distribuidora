62083bb8b5a826431f830a0f0e314c528bb9998d
Santiago <pividorisantiago@gmail.com>
2026-02-17 18:31:58 -0300

    feat(user): implement unique username generation and registration flow

--- src/main/java/com/distribuidora/urbani/service/UserService.java
+++ src/main/java/com/distribuidora/urbani/service/UserService.java
@@ -1,13 +1,18 @@
 package com.distribuidora.urbani.service;
 
 import com.distribuidora.urbani.entity.User;
+import com.distribuidora.urbani.entity.utility.Role;
+import com.distribuidora.urbani.exceptions.PasswordMismatchException;
 import com.distribuidora.urbani.repository.UserRepository;
-import lombok.AllArgsConstructor;
+import com.distribuidora.urbani.security.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 //Implementamos UserDetailsService para poder autenticar el User
 
@@ -16,6 +21,30 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(RegisterRequest registerRequest) {
+
+        if (!(registerRequest.confirmPassword().equals(registerRequest.password()))) {
+            throw new PasswordMismatchException("Passwords don't match");
+        }
+
+        return new User(
+                UUID.randomUUID(),
+                registerRequest.firstName(),
+                registerRequest.lastName(),
+                generateUsername(registerRequest.firstName(), registerRequest.lastName()),
+                passwordEncoder.encode(registerRequest.password()),
+                Role.PREVENTISTA,
+                true,
+                null,
+                null,
+                true,
+                true,
+                true,
+                true
+        );
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
