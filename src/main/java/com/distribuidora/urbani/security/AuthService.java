bac5d69a399614efa68ac13745b8a4b0953ead77
Santiago <pividorisantiago@gmail.com>
2026-02-18 09:14:51 -0300

    fix(auth): implements old register flow

--- src/main/java/com/distribuidora/urbani/security/AuthService.java
+++ src/main/java/com/distribuidora/urbani/security/AuthService.java
@@ -4,6 +4,7 @@ import com.distribuidora.urbani.entity.User;
 import com.distribuidora.urbani.repository.UserRepository;
 import com.distribuidora.urbani.security.dto.AuthResponse;
 import com.distribuidora.urbani.security.dto.LoginRequest;
+import com.distribuidora.urbani.security.dto.RegisterRequest;
 import com.distribuidora.urbani.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,17 +24,16 @@ import java.time.temporal.ChronoUnit;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final UserService userService;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-/*
     public AuthResponse register(RegisterRequest registerRequest) {
 
-        UserEntity user = userServiceImpl.register(registerRequest);
-        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        User user = userService.register(registerRequest);
 
-        String accessToken = jwtService.generateAccessToken(customUserDetails);
-        String refreshToken = jwtService.generateRefreshToken(customUserDetails);
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
 
         Instant now = Instant.now();
         user.setRefreshTokenJti(jwtService.extractJti(refreshToken));
@@ -45,10 +45,8 @@ public class AuthService {
                 refreshToken,
                 SecurityConstants.TOKEN_TYPE_BEARER,
                 now.plus(jwtService.getTokenExpirationInMs(), ChronoUnit.MILLIS),
-                user.getRefreshTokenExpirationAt(),
-                UserWebMapper.toResponse(user));
+                user.getRefreshTokenExpirationAt());
     }
-*/
 
     public AuthResponse login(LoginRequest request) {
 
