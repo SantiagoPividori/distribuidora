1fecc0c8b6c64974717eff539a50559c12c8a77b
SantiagoPividori <pividorisantiago@gmail.com>
2026-03-03 18:29:11 -0300

    refactor(client): form of search clients changed

--- src/main/java/com/distribuidora/urbani/controller/ClientController.java
+++ src/main/java/com/distribuidora/urbani/controller/ClientController.java
@@ -40,8 +40,8 @@ public class ClientController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<Client>> searchClients(@RequestParam String query) {
-        return ResponseEntity.ok(clientService.searchByNameOrCuit(query));
+    public ResponseEntity<List<Client>> searchClients(@RequestParam String name) {
+        return ResponseEntity.ok(clientService.findByName(name));
     }
 
     @GetMapping("/{id}")
