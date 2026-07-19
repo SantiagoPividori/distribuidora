6815458b6a5a9700f70b2494ec2fe3e998ea6a19
Santiago <pividorisantiago@gmail.com>
2026-07-19 04:10:07 -0300

    fix: correct global exception handling for validation and constraint errors (closes #9)

--- src/main/java/com/distribuidora/urbani/exceptions/DataIntegrityViolationException.java
+++
@@ -1,7 +0,0 @@
-package com.distribuidora.urbani.exceptions;
-
-public class DataIntegrityViolationException extends RuntimeException {
-    public DataIntegrityViolationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/distribuidora/urbani/exceptions/MethodArgumentNotValidException.java
+++
@@ -1,7 +0,0 @@
-package com.distribuidora.urbani.exceptions;
-
-public class MethodArgumentNotValidException extends RuntimeException {
-    public MethodArgumentNotValidException(String message) {
-        super(message);
-    }
-}
